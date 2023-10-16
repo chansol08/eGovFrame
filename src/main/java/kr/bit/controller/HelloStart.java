ef90675f208dc2496260aa8fa36fee2c74953964
chans <chansol8@naver.com>
2023-10-16 23:34:25 +0900

    2023-10-16
    DB 연동 sql 작성 및 객체 일부분 생성

--- src/main/java/kr/bit/controller/HelloStart.java
+++ src/main/java/kr/bit/controller/HelloStart.java
@@ -8,24 +8,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/h.do")
+@WebServlet("/h.do") /* mapping */
 public class HelloStart extends HttpServlet {
 
+    //1.클라이언트 요청을 받는 작업 => controller(servlet)
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
+        //2.처리 작업 => model(java class)
         int sum = 0;
 
         for (int i = 1; i <= 100; i++) {
             sum += i;
         }
 
+        //3.응답 작업 => view(jsp)
         PrintWriter out = resp.getWriter();
         out.println("<html>");
         out.println("<body>");
         out.println(sum);
         out.println("</body>");
-        out.println("</html>");
     }
 }
